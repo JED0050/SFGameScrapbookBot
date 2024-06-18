6cf9fc8e24c79007f1d6673546a6c66f2584fa55
JED0050 <honzonarr@seznam.cz>
2024-06-18 03:04:58 +0200

    Release v1.1 - exit button, crop size
    - fix crop rectangle (dynamic size by window resuliton)
    - exit button (q, z, esc)
    
    - todo: crop without arena timer
    - grammar
    - config file
    - ImageGrab.grab + ImageOps.crop function

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 A bot for the game Shakes and Fidget. It can be used to find items which are not registered in your scrapbook yet.
 You need to have the Real Holy Grail! without it script would not work.
 ## How to use it?
-- start your SFGame on steam
+- start your SFGame on steam in fullscreen
 - open Hall of Fame in game and start at position X
 - run python script ```bot.py```
 - write posion X from Hall of Fame to script console
@@ -11,9 +11,9 @@ You need to have the Real Holy Grail! without it script would not work.
 - positions of found players will be writen in file ```bot_output_YYYY_MM_DD_HH_mm.txt```
 - simpli find those plyers by HoF posion in txt file and kill those players for scrap credit
 ## Dependences
-Python libraries pywinauto and Pillow. Propably this script works only for windows.
+Python libraries keyboard and Pillow. Propably this script works only for windows.
 ```bash
-pip install pywinauto
+pip install keyboard
 pip install Pillow
 ```
 ## Credit
--- src/bot.py
+++ src/bot.py
@@ -4,10 +4,11 @@ from functools import reduce
 from time import sleep
 from datetime import datetime
 
-import pywinauto.keyboard as keyboard  # pip install pywinauto
+import keyboard  # pip install keyboard
 import win32gui
 from PIL import ImageGrab  # pip install Pillow
 from PIL import ImageOps
+
 
 
 def window_enum_handler(hwnd, resultList):
@@ -33,9 +34,11 @@ def compare(i1, i2):
                            map(lambda a, b: (a - b) ** 2, h1, h2)) / len(h1))
     return rms
 
+
 def write_to_file(file_name, line):
     with open(file_name, 'a') as f:
         f.write(f"{line}\n")
+
 
 def find_too_many(found_players):
     # we found 10 scraps in last 50 players (it's too many, something went wrong)
@@ -48,23 +51,34 @@ def find_too_many(found_players):
     return True
 
 
-# the main function which does all the work
 def main():
+
+    # intro talk
+    version = "1.1"
+    print("Welvome to SFGame-ScrapbookBot v" + version)
+    print("\t\t\t-by G3S")
+    print()
+    print("Place your game on main monitor and on fullscreen")
 
     # search for the Game Window and store it in the shakes variable
     appwindows = get_app_list()
-    shakes = appwindows[0][0]
+    if len(appwindows) == 0:  # SFGame app not found
+        rect = [0, 0, 1928, 1048]
+        leftcrop = 1150
+        botcrop = 400
+    else:
+        shakes = appwindows[0][0]
 
-    # set the Game Window as the active window
-    win32gui.SetForegroundWindow(shakes)
+        # set the Game Window as the active window
+        win32gui.SetForegroundWindow(shakes)
 
-    # store the coordinates which we use later to find and crop the images
-    rect = win32gui.GetWindowRect(shakes)
-    leftcrop = rect[3] // 2
-
+        # store the coordinates which we use later to find and crop the images
+        rect = win32gui.GetWindowRect(shakes)
+        leftcrop = int(rect[2] / 1.68)
+        botcrop = int(rect[3] / 2.62)
 
     # init
-    pos_hof = int(input("Hall of Fame begining position\t: "))
+    pos_hof = int(input("Hall of Fame start position\t: "))
     scan_dir = input("Scan direction, UP = 0 | DOWN = 1\t: ")
     found_players = []
 
@@ -77,39 +91,40 @@ def main():
     file_name = time.strftime("bot_output_%Y_%m_%d_%H_%M_%S.txt")
 
     if scan_dir == "1":
-        scan_dir = "{DOWN}" 
+        scan_dir = "down" 
     else:
-        scan_dir = "{UP}"
+        scan_dir = "up"
 
     old_image = None
 
 
     print(f"\nClick into game to player on HoF position {pos_hof} in next 5 seconds!")
     sleep(5)
-    print("Let's start!\n")
+    print("Bot started!\n")
 
     while pos_hof > 1 and act_scan < max_scan:
+        # Exit program
+        if keyboard.is_pressed("q") or keyboard.is_pressed("z") or keyboard.is_pressed("esc"):
+            return
+
         sleep(0.2)
 
         images = []
-
         new_image_raw = ImageGrab.grab(bbox=(rect[0], rect[1], rect[2], rect[3]))
-        new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, 0))
-        
-        #if old_image is not None:
-        #    print(compare(old_image, new_image_crop))
+        new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, botcrop))
+        #new_image_crop.save(f"crop_tmp_img_act_scan.png")
 
+        # Player is not refreshed
         if old_image is not None and compare(old_image, new_image_crop) < 50.0:    
-            #print("WE HAVE SAME PLAYER AGAIN!!!")
             for i in range(3):
                 sleep(0.175)
-                keyboard.send_keys("{DOWN}")
+                keyboard.press_and_release("down")
                 sleep(0.175)
-                keyboard.send_keys("{UP}")
+                keyboard.press_and_release("up")
             sleep(0.4)
             new_image_raw = ImageGrab.grab(bbox=(rect[0], rect[1], rect[2], rect[3]))
-            new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, 0))
-        
+            new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, botcrop))
+
         old_image = new_image_crop
         images.append(new_image_crop)
 
@@ -118,17 +133,14 @@ def main():
         for k in range(3):
             sleep(0.15)
             new_image_raw = ImageGrab.grab(bbox=(rect[0], rect[1], rect[2], rect[3]))
-            new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, 0))
+            new_image_crop = ImageOps.crop(new_image_raw, (leftcrop, 0, 0, botcrop))
             images.append(new_image_crop)
-            #new_image_crop.save(f"img_{k}.png")
             sleep(0.15)
 
             for i in range(k+1):
                 new_diff = compare(images[i], new_image_crop)
                 if new_diff > max_diff:
                     max_diff = new_diff
-                    #images[i].save(f"img_{pos_hof}_{i}.png")
-                    #images[j].save(f"img_{pos_hof}_{j}.png")
                     if max_diff > min_diff_scrap:
                         break
             
@@ -137,6 +149,8 @@ def main():
 
         # compute the difference and print it to the console
         print(f"Pos: {pos_hof}\tDiff: {max_diff}\tFound: {found_players}")
+        if act_scan % 100 == 0:
+            print("\tpress 'q' or 'esc' to exit program")
 
         # if the Images are different enough the loop will break since the Bot expects to have found an unknown item
         # if the Images are the same the bot will increment its counter and go up to the next player
@@ -148,15 +162,15 @@ def main():
         if find_too_many(found_players):
             break
 
-        keyboard.send_keys(scan_dir)
+        keyboard.press_and_release(scan_dir)
         act_scan += 1
-        if scan_dir == "{UP}":
+        if scan_dir == "up":
             pos_hof -= 1
         else:
             pos_hof += 1
 
-    # After the Bot has found an item it will output some information and the user can exit the program by hitting Enter
     print(f"Players with items to scrapbook: {found_players}")
+
 
 if __name__ == "__main__":
     main()
