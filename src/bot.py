e6e9358b17e806fde9e5e078ee560fe6517d6186
JED0050 <honzonarr@seznam.cz>
2024-06-19 17:19:59 +0200

    v1.2.5 - config file impact, logger, gitignore debug folders, found scrap folder
    - TODO: - grammar comments + readme
    - exe file final

--- src/bot.py
+++ src/bot.py
@@ -4,7 +4,8 @@ from functools import reduce
 from time import sleep
 from datetime import datetime
 import logging
-import numbers
+import os
+import sys
 
 from PIL import ImageGrab, ImageOps, Image  # pip install Pillow
 import keyboard  # pip install keyboard
@@ -18,34 +19,38 @@ class Config:
         self.config_dict = {}
         self._set_default_values()
 
-        
-        with open(file_name, "r") as f:
-            for line in f:
-                line = line.replace(" ", "")[:-1]
-                if line.startswith("#") or len(line) < 3:
-                    continue
-                line_split = line.split("=")
-                if len(line_split) != 2:
-                    continue
-                key = line_split[0].upper()
-                value = line_split[1].upper()
-                value = self._parse_value(value)
-                self.config_dict[key] = value
-                if self.config_dict["SETTINGS"] == False:
-                    return
-
-        self._set_file_values()
-
-        logger.disabled = True if self.config_dict["DEBUG_ON"] is False else False
-        logger.debug("CONFIG FILE DONE")
-        logger.debug(f"MASK={self.MASK}")
-        logger.debug(f"WINDOW_SIZE={self.WINDOW_SIZE}")
-        logger.debug(f"HOF_POSITION={self.HOF_POSITION}")
-        logger.debug(f"SCAN_DIRECTION_UP={self.SCAN_DIRECTION_UP}")
-        logger.debug(f"FILE_SAVE={self.FILE_SAVE}")
-        logger.debug(f"SPEED={self.SPEED}")
-        logger.debug(f"SCAN_LIMIT={self.SCAN_LIMIT}")
-        logger.debug(f"DIFF_LIMIT={self.DIFF_LIMIT}")
+        try:
+            with open(file_name, "r") as f:
+                for line in f:
+                    line = line.replace(" ", "")[:-1]
+                    if line.startswith("#") or len(line) < 3:
+                        continue
+                    line_split = line.split("=")
+                    if len(line_split) != 2:
+                        continue
+                    key = line_split[0].upper()
+                    value = line_split[1].upper()
+                    value = self._parse_value(value)
+                    self.config_dict[key] = value
+                    if self.config_dict["SETTINGS"] == False:
+                        return
+        except Exception as e:
+            logger.warning(f"Reading config file - {e}")
+        finally:
+            self._set_file_values()
+            debug_off = True if self.config_dict["DEBUG_ON"] is False else False
+            self.DEBUG_ON = not debug_off
+            logger.disabled = debug_off
+            logger.debug("Logger is turned on for debugging")
+            logger.debug("CONFIG FILE DONE")
+            logger.debug(f"MASK={self.MASK}")
+            logger.debug(f"WINDOW_SIZE={self.WINDOW_SIZE}")
+            logger.debug(f"HOF_POSITION={self.HOF_POSITION}")
+            logger.debug(f"SCAN_DIRECTION_UP={self.SCAN_DIRECTION_UP}")
+            logger.debug(f"FILE_SAVE={self.FILE_SAVE}")
+            logger.debug(f"SPEED={self.SPEED}")
+            logger.debug(f"SCAN_LIMIT={self.SCAN_LIMIT}")
+            logger.debug(f"DIFF_LIMIT={self.DIFF_LIMIT}")
 
 
     def _set_default_values(self):
@@ -55,6 +60,7 @@ class Config:
         self.SCAN_DIRECTION_UP = None
         self.FILE_SAVE = True
         self.SPEED = 1
+        self.DEBUG_ON = False
         self.SCAN_LIMIT = 5000
         self.DIFF_LIMIT = 3.0
 
@@ -65,7 +71,7 @@ class Config:
         self.config_dict["SCAN_DIRECTION_UP"] = None
         self.config_dict["FILE_SAVE"] = None
         self.config_dict["SPEED"] = None
-        self.config_dict["DEBUG_ON"] = True  # False
+        self.config_dict["DEBUG_ON"] = False
         self.config_dict["SCAN_LIMIT"] = None
         self.config_dict["DIFF_LIMIT"] = None
 
@@ -121,9 +127,9 @@ class Config:
             self.WINDOW_SIZE = self.config_dict["WINDOW_SIZE"]
         if isinstance(self.config_dict["HOF_POSITION"], float):
             number = int(self.config_dict["HOF_POSITION"])
-            if number < 0:
-                self.HOF_POSITION = None
-            if number > 1000000000:
+            if number < 1:
+                number = None
+            elif number > 1000000000:
                 number = 1000000000
             self.HOF_POSITION = number
         if isinstance(self.config_dict["SCAN_DIRECTION_UP"], bool):
@@ -133,21 +139,21 @@ class Config:
         if isinstance(self.config_dict["SPEED"], float):
             number = self.config_dict["SPEED"]
             if number < 0:
-                pass
+                number = 1
             elif number > 10:
                 number = 10
             self.SPEED = number
         if isinstance(self.config_dict["SCAN_LIMIT"], float):
             number = int(self.config_dict["SCAN_LIMIT"])
             if number < 10:
-                pass
-            if number > 1000000000:
+                number = 5000
+            elif number > 1000000000:
                 number = 1000000000
             self.SCAN_LIMIT = number
         if isinstance(self.config_dict["DIFF_LIMIT"], float):
             number = self.config_dict["DIFF_LIMIT"]
             if number < 0.1:
-                pass
+                number = 3.0
             elif number > 1000:
                 number = 1000
             self.DIFF_LIMIT = number
@@ -194,14 +200,21 @@ def find_too_many(found_players):
     return True
 
 
-def crop_screenshot(rect, cropbox, black_bg_img, mask_img):
+def crop_screenshot(rect, cropbox, black_bg_img, mask_img, config):
     raw_img = ImageGrab.grab(bbox=(rect[0], rect[1], rect[2], rect[3]))  # Take whole screenshot
-    #raw_img.save("raw_img_debug.png")
     crop_img = ImageOps.crop(raw_img, cropbox)  # Crop data from it (right up corner)
-    #crop_img.save("crop_img_debug.png")
-    masked_img = Image.composite(crop_img, black_bg_img, mask_img)  # Mask data (timer, guild chat, xp bar)
-    #masked_img.save("masked_img_debug.png")
-    return masked_img
+
+    if config.DEBUG_ON:
+        raw_img.save("DEBUG/raw_img_debug.png")
+        crop_img.save("DEBUG/crop_img_debug.png")
+    if config.MASK:
+        masked_img = Image.composite(crop_img, black_bg_img, mask_img)  # Mask data (timer, guild chat, xp bar)
+        if config.DEBUG_ON:
+            masked_img.save("DEBUG/masked_final_img_debug.png")
+        return masked_img
+    
+    return crop_img
+
 
 
 def main():
@@ -219,82 +232,131 @@ def main():
     config = Config(config_file_name, logger)
 
     # intro talk
-    version = "1.2.2.5"
+    version = "1.2.5"
     print("Welvome to SFGame-ScrapbookBot v" + version)
     print("\t\t\t-by Gaara3San")
     print()
     print("Place your game on main monitor and on fullscreen")
 
     # search for the Game Window and store it in the shakes variable
-    appwindows = get_app_list()
-    if len(appwindows) == 0:  # SFGame app not found
-        rect = [0, 0, 1928, 1048]
-        leftcrop = 1150
-        topcrop = 35
-        rightcrop = 50
-        botcrop = 450
-        logger.warning("Steam game not found")
+    if isinstance(config.WINDOW_SIZE, tuple):
+        rect = [0, 0, config.WINDOW_SIZE[0], config.WINDOW_SIZE[1]]
     else:
-        shakes = appwindows[0][0]
-        # set the Game Window as the active window
-        win32gui.SetForegroundWindow(shakes)
-        # store the coordinates which we use later to find and crop the images
-        rect = win32gui.GetWindowRect(shakes)
-        leftcrop = int(rect[2] / 1.68)
-        topcrop = int(rect[3] / 29.94)
-        rightcrop = int(rect[2] / 38.56)
-        botcrop = int(rect[3] / 2.49)
+        appwindows = get_app_list()
+        if len(appwindows) == 0:  # SFGame app not found
+            rect = [0, 0, 1928, 1048]
+            logger.warning("Steam game not found")
+        else:
+            shakes = appwindows[0][0]
+            # set the Game Window as the active window
+            win32gui.SetForegroundWindow(shakes)
+            # store the coordinates which we use later to find and crop the images
+            rect = win32gui.GetWindowRect(shakes)
+    if rect[2] < 10 or rect[3] < 10:
+            logger.warning("Game is minimalized or invalid config - too small size values")
+            rect = [0, 0, 1928, 1048]
+    leftcrop = int(rect[2] / 1.68)
+    topcrop = int(rect[3] / 29.94)
+    rightcrop = int(rect[2] / 38.56)
+    botcrop = int(rect[3] / 2.49)
+
     cropbox = (leftcrop, topcrop, rightcrop, botcrop)
     init_raw_img = ImageGrab.grab(bbox=(rect[0], rect[1], rect[2], rect[3]))
+    logger.debug(f"Cropbox size: {cropbox}, Screenshot crop size: {init_raw_img.size}")
     init_crop_img = ImageOps.crop(init_raw_img, cropbox)
-    mask_img = Image.open('mask.png').convert("L").resize(init_crop_img.size)
-    black_bg_img = Image.new("RGB", init_crop_img.size, (0, 0, 0))
-    logger.debug(f"Cropbox size: {cropbox}, Screenshot crop size: {init_crop_img.size}")
+    if not logger.disabled:
+        logger.debug("Debug folder created")
+        if not os.path.exists("DEBUG"):
+            os.makedirs("DEBUG")
+    mask_img = None
+    black_bg_img = None
+    if config.MASK:
+        try:
+            mask_img = Image.open('mask.png').convert("L").resize(init_crop_img.size)
+            black_bg_img = Image.new("RGB", init_crop_img.size, (0, 0, 0))
+            if not logger.disabled:
+                logger.debug("Saving mask and black images")
+                mask_img.save("DEBUG/mask_img_debug.png")
+                black_bg_img.save("DEBUG/black_bg_img_debug.png")
+        except Exception as e:
+            logger.warning(f"Problem with mask - {e}")
+            config.MASK = False
 
     # init
-    pos_hof = int(input("Hall of Fame start position\t\t: "))
-    scan_dir = input("Scan direction [UP↑ = 0 | DOWN↓ = 1]\t: ")
+    if config.HOF_POSITION is None or not isinstance(config.HOF_POSITION, int):
+        while True:
+            try:
+                pos_hof = int(input("Hall of Fame start position\t\t: "))
+                if pos_hof < 1:
+                    pos_hof = 1
+                elif pos_hof > 1000000000:
+                    pos_hof = 1000000000
+                break
+            except Exception as e:
+                logger.warning("Invalid Hall of Fame position")
+                print(f"\tInsert integer number - {e}")
+    else:
+        logger.debug("Hall of Fame position found in config file")
+        pos_hof = config.HOF_POSITION
+    
+    if config.SCAN_DIRECTION_UP is None:
+        scan_dir = input("Scan direction [UP↑ = 0 | DOWN↓ = 1]\t: ")
+        if scan_dir == "1":
+            scan_dir = "down"
+        else:
+            scan_dir = "up"
+    else:
+        logger.debug("Scan direction found in config file")
+        if config.SCAN_DIRECTION_UP is False:
+            scan_dir = "down"
+        else:
+            scan_dir = "up"
+
     found_players = []
 
     act_scan = 0
-    max_scan = 10000
+    max_scan = config.SCAN_LIMIT
+    min_diff_scrap = config.DIFF_LIMIT
+    speed = 1/config.SPEED
+    if speed != 1:
+        logger.debug("Speed value changed to {config.SPEED}")
 
-    min_diff_scrap = 3.0
+    if config.FILE_SAVE:
+        logger.debug("Saving player's positions to file")
+        time = datetime.now()
+        file_name = time.strftime("bot_output_%Y_%m_%d_%H_%M_%S.txt")
 
-    time = datetime.now()
-    file_name = time.strftime("bot_output_%Y_%m_%d_%H_%M_%S.txt")
-
-    if scan_dir == "1":
-        scan_dir = "down" 
-    else:
-        scan_dir = "up"
+    wait_time = 5
+    print()
+    for sec in range(wait_time, 0, -1):
+        sys.stdout.write(f"\rClick into game to player on HoF position {pos_hof} in next {sec} seconds!")
+        sys.stdout.flush()
+        sleep(1)
+    print("\nBot started!")
 
     old_image = None
 
-
-    print(f"\nClick into game to player on HoF position {pos_hof} in next 5 seconds!")
-    sleep(5)
-    print("\nBot started!")
-
-    while pos_hof > 1 and act_scan < max_scan:
+    while pos_hof >= 1 and act_scan < max_scan:
         # Exit program
         if keyboard.is_pressed("q") or keyboard.is_pressed("z") or keyboard.is_pressed("esc"):
-            return
+            logger.debug("User exits script")
+            break
 
-        sleep(0.2)
+        sleep(speed*0.2)
 
         images = []
-        new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img)
+        new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img, config)
 
         # Player is not refreshed
-        if old_image is not None and compare(old_image, new_image_crop) < 50.0:    
+        if old_image is not None and compare(old_image, new_image_crop) < 50.0:
+            logger.debug(f"Player {pos_hof} is not refreshed")
             for i in range(3):
-                sleep(0.175)
+                sleep(speed*0.175)
                 keyboard.press_and_release("down")
-                sleep(0.175)
+                sleep(speed*0.175)
                 keyboard.press_and_release("up")
-            sleep(0.4)
-            new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img)
+            sleep(speed*0.4)
+            new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img, config)
 
         old_image = new_image_crop
         images.append(new_image_crop)
@@ -302,13 +364,14 @@ def main():
         max_diff = 0.0
 
         for k in range(3):
-            sleep(0.15)
-            new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img)
+            sleep(speed*0.15)
+            new_image_crop = crop_screenshot(rect, cropbox, black_bg_img, mask_img, config)
             images.append(new_image_crop)
-            sleep(0.15)
+            sleep(speed*0.15)
 
             for i in range(k+1):
                 new_diff = compare(images[i], new_image_crop)
+                logger.debug(f"Taking screenshot {k+1} {i+1} player {pos_hof} - new difference {new_diff}")
                 if new_diff > max_diff:
                     max_diff = new_diff
                     if max_diff > min_diff_scrap:
@@ -325,11 +388,14 @@ def main():
         # if the Images are different enough the loop will break since the Bot expects to have found an unknown item
         # if the Images are the same the bot will increment its counter and go up to the next player
         if max_diff > min_diff_scrap:
+            logger.debug(f"Adding player {pos_hof} to collection")
             found_players.append(pos_hof)
             print(f"Player with missing item found! Position: {pos_hof}")
-            write_to_file(file_name, pos_hof)
+            if config.FILE_SAVE is True:
+                write_to_file(file_name, pos_hof)
 
         if find_too_many(found_players):
+            logger.debug("Too many players with new scrap in row found")
             break
 
         keyboard.press_and_release(scan_dir)
@@ -338,8 +404,15 @@ def main():
             pos_hof -= 1
         else:
             pos_hof += 1
-
-    print(f"Players with items to scrapbook: {found_players}")
+    
+    logger.debug("Main loop exited")
+    if len(found_players) == 0:
+        logger.debug("0 players found")
+        print("0 players found :(")
+    else:
+        logger.debug(f"{len(found_players)} players found")
+        print(f"{len(found_players)} players with items to scrapbook found")
+        print(f"Player's Hall of Fame positions:{found_players}")
 
 
 if __name__ == "__main__":
